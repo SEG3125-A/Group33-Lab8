3762607d40060aa36c84763689b327d244ed8d79
katemilt <kaitlynmiltimore@gmail.com>
2024-03-17 20:24:48 -0400

    Trying to start a navbar

--- src/App.js
+++ src/App.js
@@ -2,7 +2,31 @@ import './App.css';
 
 function App() {
   return (
+
     <div className="App">
+      <nav className="navbar">
+        <div className="container">
+          {/* <img src={logo} className="navbar-logo" alt="logo" /> */}
+          <ul className="navbar-links">
+            <li><a href="#">Home</a></li>
+            <li><a href="#">About</a></li>
+            <li><a href="#">Our Tutors</a></li>
+            <li>
+              <div className="dropdown">
+                <button className="dropbtn">Courses</button>
+                <div className="dropdown-content">
+                  <a href="#">Add courses here</a>
+                  <a href="#">...</a>
+                  <a href="#">...</a>
+                  <a href="#">...</a>
+                </div>
+              </div>
+            </li>
+            <li><a href="#">Contact</a></li>
+          </ul>
+        </div>
+      </nav>
+
       <header className="App-header">
         <h1>
         Welcome to UottawaFriends.com!

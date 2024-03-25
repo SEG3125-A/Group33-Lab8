16bc8426af66d693fccdd24d43503e82c2b9b621
Egerm010 <90728977+Egerm010@users.noreply.github.com>
2024-03-24 23:36:55 -0400

    Update Contact.js

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -3,8 +3,11 @@ import React from 'react';
 function Contact() {
   return (
     <div>
-      <h1>About</h1>
-      {/* Add content for page here */}
+      <h1>Need a hand with how the website works? Give the number below a call, or send a message to the email below!</h1>
+      Customer Service Guide Phone Number: 907-122-0925
+      Customer Service Guide Email: uottawafriendsguide@uottawa.ca
+      <h1>Encounter any technical bugs while using the website? Give the email a message below to try and get it fixed!</h1>
+      Technical Support Email: uottawafriendstech@uottawa.ca
     </div>
   );
 }
